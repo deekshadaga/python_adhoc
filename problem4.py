45957facc921c41c95aa858a23682c4bca29aeec
deekshadaga <51335229+deekshadaga@users.noreply.github.com>
2019-06-10 16:41:10 +0000

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -1,3 +1,11 @@
 #!usr/bin/python3
 
+import os
 
+uname = input("enter username ---> ")
+if type(uname)==str:
+	upass="hello"+uname
+	os.system("useradd -p $(openssl passwd -1 "+upass+") "+uname)
+	print("user created ! ")
+else:
+	print("invalid user name")
